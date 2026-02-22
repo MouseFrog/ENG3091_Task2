75520b0a25ddb17858f89ba02e4af30ebd6e504f
MouseFrog <kluilin108@gmail.com>
2026-02-22 20:30:55 +0000

    code generating generic house prices with equation form y=mx+c

--- noise.cpp
+++ noise.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <fstream>  // Required for file operations
+#include <random>
+
+    std::random_device device_num; // Random number generated from hardware 
+    std::mt19937 mt_num(device_num()); // mt19937: Mersenne Twister pseudo-random generator 
+    std::uniform_int_distribution<> distribute(1, 5);  // Value range 1 to 5
+
+    // std::uniform_real_distribution<double> distr(1,900) // for price --> double and no integer
 
 int main() {
     // Initialise vector "house_prices" with data type double 
-    std::vector<double> house_prices = {250.5, 300.2, 150.7, 420.0, 310.3};
+    std::vector<double> house_prices(100); // 100 points
+    // Initialise bedroom number vector 
+    std::vector <double> bedroom(100); // 100 points
+
+    // Generate bedroom data points
+    for (int i=0; i<100; i++) {
+        bedroom[i]=distribute(mt_num);
+        house_prices[i] = 50*bedroom[i]+100;
+    }
+    
 
     // Open a file named "prices.csv"
     std::ofstream myFile("prices.csv");
 
     // Check if file opened correctly
     if (myFile.is_open()) {
-        // Add header (optional, but good for Python/Excel)
-        myFile << "Price" << std::endl;
+        // Add header 
+        // Comma used as separator, "Number of Bedrooms" prints in next column
+        // .csv file stands for "comma separated values"
+        myFile << "Price" << "," << "Number of Bedrooms" <<std::endl;
 
-        // Range based loop: 1st iteration: val = first value in house_price, 2nd iteration: val = second value in house_price
-        for (double val : house_prices) {
-            myFile << val << std::endl; 
+        // Write house prices and bedroom # vectors into file
+        for (int i=0; i<100; i++) {
+            myFile << house_prices[i]<< "," <<bedroom[i] << std::endl;
         }
 
         // Close file
