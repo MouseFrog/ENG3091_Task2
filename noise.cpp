6c57ea46ea6d94106869f7821ef4ade9ad675b00
MouseFrog <kluilin108@gmail.com>
2026-02-22 20:36:37 +0000

    more comments

--- noise.cpp
+++ noise.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <vector>
+#include <vector>   // for std::vector
 #include <fstream>  // Required for file operations
-#include <random>
+#include <random>   // For data generation
 
     std::random_device device_num; // Random number generated from hardware 
     std::mt19937 mt_num(device_num()); // mt19937: Mersenne Twister pseudo-random generator 
@@ -10,27 +10,24 @@
     // std::uniform_real_distribution<double> distr(1,900) // for price --> double and no integer
 
 int main() {
-    // Initialise vector "house_prices" with data type double 
-    std::vector<double> house_prices(100); // 100 points
-    // Initialise bedroom number vector 
-    std::vector <double> bedroom(100); // 100 points
+    // Initialise vectors with data type double, with 100 points
+    std::vector<double> house_prices(100); 
+    std::vector <double> bedroom(100); 
 
     // Generate bedroom data points with equation form y = mx + c
     for (int i=0; i<100; i++) {
         bedroom[i]=distribute(mt_num);
         house_prices[i] = 50*bedroom[i]+100; // m = 50, c = 100
     }
-    
 
     // Open a file named "prices.csv"
     std::ofstream myFile("prices.csv");
 
     // Check if file opened correctly
-    if (myFile.is_open()) {
-        // Add header 
-        // Comma used as separator, "Number of Bedrooms" prints in next column
-        // .csv file stands for "comma separated values"
-        myFile << "Price" << "," << "Number of Bedrooms" <<std::endl;
+    if (myFile.is_open()) { 
+        // Add headers for clarity 
+        // .csv file stands for "comma separated values" --> "Number of Bedrooms" will print in next column
+        myFile << "Price" << "," << "Number of Bedrooms" <<std::endl; // Use comma as separator
 
         // Write house prices and bedroom # vectors into file
         for (int i=0; i<100; i++) {
