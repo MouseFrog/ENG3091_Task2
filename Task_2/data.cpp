fa83cfe666ec834d3b23675ff6b1fea0046ff258
MouseFrog <kluilin108@gmail.com>
2026-03-01 21:26:30 +0000

    bedroom weight 5000 to 50000 for realism

--- Task_2/data.cpp
+++ Task_2/data.cpp
@@ -5,22 +5,17 @@
 
 
     
-    // Calculate appropriate noise distribution level based on average house price
+    // Adds noise to each individual data point
     void calc_noise(std::vector <double>& data_in, std::mt19937& mt_num){ // Pass by reference alters original dataset, return nothing
         double current_sum = 0;
         for (int i=0; i<data_in.size(); i++){
-            current_sum = current_sum + data_in[i];
-        }
-        double average_val = current_sum/data_in.size();
-        double standard_dev= average_val*0.05;
-        std::cout<<"average price: "<<average_val<<std::endl;
-        // Gaussian White Noise
-        std::normal_distribution<double> noise_dist(0.0, standard_dev); // Mean = 0.0, standar dev to be calculated
-
-        for (int i=0; i<data_in.size(); i++){
+            double indiv_dev = data_in[i] * 0.05;   // Calc standard deviation for each point to apply noise
+            // Gaussian White Noise
+            std::normal_distribution<double> noise_dist(0.0, indiv_dev);
             double noise = noise_dist(mt_num);
             data_in[i]=data_in[i]+noise;
         }
+        
     }
 
 
@@ -28,16 +23,16 @@ int main() {
 
     // Set up random data generation
     std::random_device device_num; // Random number generated from hardware 
-    std::mt19937 mt_num(device_num()); // mt19937: Mersenne Twister pseudo-random generator 
-    // Generate data for variables
+    std::mt19937 mt_num(device_num()); // Mersenne Twister pseudo-random generator 
+
+    // Generate distributed values for variables
     std::uniform_int_distribution<> room_dist(1, 5);  // Bedroom values range 1 to 5
     std::uniform_int_distribution<> area_dist(25, 1000);  // Land area range 25m^2 to 1500m^2
 
     // Setup matrix for storing value of variables
     int data_points = 100;  // row: # of data points
     int num_variables = 3; // column: y-intercept, land size, # of bedrooms
-    // 100x3 matrix, initialise all points with value of 0.0
-    std::vector<std::vector<double>> raw_data(data_points, std::vector<double>(num_variables, 0.0)); 
+    std::vector<std::vector<double>> raw_data(data_points, std::vector<double>(num_variables, 0.0)); // 100x3 matrix, initialise all points with value of 0.0
 
     // Initialise vector for calculating house prices
     std::vector <double> single_price(100); // Simple Linear Regression
@@ -49,8 +44,9 @@ int main() {
     // Single variable regression slope
     //const double m = 10000;
     // Multi-variable regression weights
-    const double W_bedroom = 5000;    // 5000 pounds per bedroom added
     const double W_area = 1600; // 1600 pounds per square meter 
+    const double W_bedroom = 50000;    // pounds per bedroom added
+
 
 
     for (int i=0; i<100; i++) {
