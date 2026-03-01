969efbfd21f28ec1826b97d017ee630676a126f8
MouseFrog <kluilin108@gmail.com>
2026-03-01 12:43:30 +0000

    moved noise.cpp into folder for simple regression

--- simple_reg/noise.cpp
+++ simple_reg/noise.cpp
@@ -7,19 +7,27 @@
     std::mt19937 mt_num(device_num()); // mt19937: Mersenne Twister pseudo-random generator 
     std::uniform_int_distribution<> distribute(1, 5);  // Value range 1 to 5
 
-    // std::uniform_real_distribution<double> distr(1,900) // for price --> double and no integer
+    // std::uniform_real_distribution<type> name(min,max) // for price --> double and no integer
 
 int main() {
+
+    int data_points = 100;  // rows: # of data points
+    int num_variables = 3; // columns: land size, # of bedrooms, y-intercept
+
+    // 100x2 matrix, initialise all points with value of 0.0
+    std::vector<std::vector<double>> raw_data(data_points, std::vector<double>(num_variables, 0.0)); 
+/*
     // Initialise vectors with data type double, with 100 points
     std::vector<double> house_prices(100); 
     std::vector <double> bedroom(100); 
+
 
     // Generate bedroom data points with equation form y = mx + c
     for (int i=0; i<100; i++) {
         bedroom[i]=distribute(mt_num);
         house_prices[i] = 50*bedroom[i]+100; // m = 50, c = 100
     }
-
+*/
     // Open a file named "prices.csv"
     std::ofstream myFile("prices.csv");
 
@@ -31,7 +39,7 @@ int main() {
 
         // Write house prices and bedroom # vectors into file
         for (int i=0; i<100; i++) {
-            myFile << house_prices[i]<< "," <<bedroom[i] << std::endl;
+            myFile << house_prices[i]<< "," <<bedroom[i] << std::endl; // Comma as separator to print in respective column
         }
 
         // Close file
