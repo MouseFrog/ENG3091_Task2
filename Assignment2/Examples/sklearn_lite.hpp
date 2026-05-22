d8e5ad61ee9d15226a41342c0e99c4835a2dd88a
MouseFrog <kluilin108@gmail.com>
2026-05-22 14:37:27 +0100

    deleted old linear.cpp, updated sklearn_lite.hpp include file paths

--- Assignment2/Examples/sklearn_lite.hpp
+++ Assignment2/Examples/sklearn_lite.hpp
@@ -3,7 +3,9 @@
 #ifndef SKLEARN_LITE_HPP
 #define SKLEARN_LITE_HPP
 
+#include "../include/sklearn_lite/linear_models/BaseModel.hpp"
 #include "../include/sklearn_lite/linear_models/LogisticRegression.hpp"
+#include "../include/sklearn_lite/linear_models/LinearRegression.hpp"
 #include "../include/sklearn_lite/Utils.hpp"
 
 #endif
